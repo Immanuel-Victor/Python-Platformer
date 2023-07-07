20c3cdd30386078da869287bf8f8a70323a57aef
Immanuel-Victor <immanuelvictor.a.f@gmail.com>
2023-07-07 11:52:04 -0300

    feat: adiciona renderizacao de background

--- game.py
+++ game.py
@@ -15,9 +15,28 @@ FPS = 60
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def get_background(bg_name):
+    image = pygame.image.load(join("assets","Background",bg_name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH//width + 1):
+        for j in range(HEIGHT//height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles,image
+
+def draw(window,background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
 
 def main(window):
     time = pygame.time.Clock()
+    background,bg_image = get_background("Brown.png")
+
     run = True
 
     while run:
@@ -27,6 +46,7 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw(window,background,bg_image)
 
     pygame.quit()
     quit()
